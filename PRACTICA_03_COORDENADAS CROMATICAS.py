cefdc27f2048d9e42e2b766fdd58e9a91c0dcd7a
hector-ortega <117932913+hector-ortega@users.noreply.github.com>
2024-03-11 22:37:11 -0600

    Update PRACTICA_03_COORDENADAS CROMATICAS.py

--- PRACTICA_03_COORDENADAS CROMATICAS.py
+++ PRACTICA_03_COORDENADAS CROMATICAS.py
@@ -6,6 +6,11 @@ imageno1 = imagen.copy()
 imageno2 = imagen.copy()
 # imageno3 = cv2.imread("mano_o3.png")
 
+#184,83,61
+#147,98,82
+#136,103,89
+#119,107,103
+
 m,n,c = imagen.shape
 
 
@@ -16,17 +21,17 @@ imageno1 = imageno1.astype(np.float32)
 imageno2 = imageno2.astype(np.float32)
 # imageno3 = imageno3.astype(np.float32)
 
-def clasificador (imagen):
+def clasificador (imagen,s):
     m,n,c = imagen.shape
     imagen_b=np.zeros((m,n))
     for x in range (m):
         for y in range (n):
-            if 50<imagen[x,y,0]<232 and imagen[x,y,1]<232 and imagen[x,y,2]<232:
+            if 50<imagen[x,y,0]<105 and imagen[x,y,1]<110 and imagen[x,y,2]<200:
                 imagen_b[x,y]=255
                     
-    #cv2.imwrite("imagen_b1.jpg",imagen_b)
-    cv2.imshow("mano binaria",imagen_b)
-    cv2.waitKey(0)
+
+    cv2.imshow(s,imagen_b)
+    cv2.imwrite(s+".png",imagen_b)
     return imagen_b
 
 def cromatico(imagenc,s,crom):
@@ -42,7 +47,6 @@ def cromatico(imagenc,s,crom):
             imagenc[x,y,2] = imagen[x,y,2]/(imagen[x,y,0]+imagen[x,y,1]+imagen[x,y,2])
 
     cv2.imshow(crom,imagenc)
-    cv2.waitKey(0)
     imagenc = cv2.normalize(imagenc, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
     cv2.imwrite(crom+".png",imagenc)
     return imagenc
@@ -50,65 +54,36 @@ def cromatico(imagenc,s,crom):
 #-----------SE CREAN LAS IMAGENES CON DIFERENTE OPACIDAD-----------
 imagen = cv2.imread("mano.jpg")
 cv2.imshow("mano OG",imagen)
-cv2.waitKey(0)#espera a aque se presione la X
-cv2.destroyAllWindows() #se cierra la ventana al presionar la X
 
 imageno1 = imagen*0.7 #se multiplica por un fator para modificar su nitidez o exposicion
 imageno1 = imageno1.astype(np.uint8) #Al multiplicar por el numero flotante se convierte en flotante
 cv2.imshow("mano obscura 1",imageno1) #Se muestra la nueva imagen oscura
-cv2.waitKey(0)#espera a aque se presione la X
-cv2.destroyAllWindows() #se cierra la ventana al presionar la X
 cv2.imwrite("mano_o1.png",imageno1)
 
 imageno2 = imagen*0.3
 imageno2 = imageno2.astype(np.uint8)
 cv2.imshow("mano obscura 2",imageno2) #Se muestra la nueva imagen oscura
-cv2.waitKey(0)#espera a aque se presione la X
-cv2.destroyAllWindows() #se cierra la ventana al presionar la X
 cv2.imwrite("mano_o2.png",imageno2)
 
-
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 #imagen = cv2.imread("mano.jpg") #se guarda la imagen en la variable imagen
 
 imagenc = cromatico(imagenc,"mano.jpg","imagen_cromatica01")
 imageno1 = cromatico(imageno1,"mano_o1.png","imagen_cromatica02")
 imageno2 = cromatico(imageno2, "mano_o2.png","imagen_cromatica03")
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-imagenc = cv2.imread("imagen_cromatica01.png")
-imageno1 = cv2.imread("imagen_cromatica02.png")
-imageno2 = cv2.imread("imagen_cromatica03.png")
+# imagenc = cv2.imread("imagen_cromatica01.png")
+# imageno1 = cv2.imread("imagen_cromatica02.png")
+# imageno2 = cv2.imread("imagen_cromatica03.png")
 
-imagenc = clasificador(imagenc)
-imageno1 = clasificador(imageno1)
-imageno2 = clasificador(imageno2)
-
+imagenc = clasificador(imagenc,"clasificador_C01")
+imageno1 = clasificador(imageno1,"clasificador_C02")
+imageno2 = clasificador(imageno2,"clasificador_C03")
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 #espera a aque se presione la X
 cv2.destroyAllWindows()
 
-
-
-#-----------SE CREAN LAS IMAGENES CON DIFERENTE OPACIDAD-----------
-
-# imageno1 = imagen*0.3 #se multiplica por un fator para modificar su nitidez o exposicion
-# imageno1 = imageno1.astype(np.uint8) #Al multiplicar por el numero flotante se convierte en flotante
-# cv2.imshow("mano obscura",imageno1) #Se muestra la nueva imagen oscura
-# cv2.waitKey(0)#espera a aque se presione la X
-# cv2.destroyAllWindows() #se cierra la ventana al presionar la X
-# cv2.imwrite("mano_o1.png",imageno1)
-
-# imageno2 = imagen*0.5
-# imageno2 = imageno2.astype(np.uint8)
-# cv2.imshow("mano obscura 2",imageno2) #Se muestra la nueva imagen oscura
-# cv2.waitKey(0)#espera a aque se presione la X
-# cv2.destroyAllWindows() #se cierra la ventana al presionar la X
-# cv2.imwrite("mano_o2.png",imageno2)
-
-# imageno3 = imagen*0.8
-# imageno3 = imageno3.astype(np.uint8)
-# cv2.imshow("mano obscura 3",imageno3) #Se muestra la nueva imagen oscura
-# cv2.waitKey(0)#espera a aque se presione la X
-# cv2.destroyAllWindows() #se cierra la ventana al presionar la X
-# cv2.imwrite("mano_o3.png",imageno3)
-
-# cv2.waitKey(0)#espera a aque se presione la X
-# cv2.destroyAllWindows() #se cierra la ventana al presionar la X
